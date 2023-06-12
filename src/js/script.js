6094639d3f01a225b6e7c3e99d2545707e83e27b
Sergiy <33hantkun33@gmail.com>
2023-06-12 14:36:00 +0300

    Finaly project

---
+++ src/js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    // infinite: true,
+    speed: 1200,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow_left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_right.png"></button>',
+    responsive : [ 
+      {
+          breakpoint: 991,
+          settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item){
+    $(item).each(function(i){
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    });
+  };
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  //modal
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+  });
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  });
+
+  //SMOTH scrol
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 1300){
+      $('.pageup').fadeIn()
+    }else{
+      $('.pageup').fadeOut();
+    }
+  });
+
+    $("a[href^='#'").click(function() {
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+    new WOW().init();
+});
